fa9cc24883589de196e291193d0bd60836375dec
SlivkaEvgen <79520449+SlivkaEvgen@users.noreply.github.com>
2021-10-20 19:31:08 +0300

    Update Validator.java

--- src/main/java/org/homework/util/Validator.java
+++ src/main/java/org/homework/util/Validator.java
@@ -37,7 +37,6 @@ public class Validator<T extends BaseModel<ID>, ID> {
     }
 
     public static boolean validName(String name) {
-//return name.equalsIgnoreCase("/^[a-z0-9_-]{3,16}$/");
         if (name.isEmpty()) {
             return false;
         }
@@ -51,10 +50,6 @@ public class Validator<T extends BaseModel<ID>, ID> {
             return false;
         }
         return validString(name);
-    }
-
-    public static boolean validLength(String parameter) {
-        return parameter.length() >= 12;
     }
 
     public static boolean validActivity(String activity) {
@@ -80,59 +75,11 @@ public class Validator<T extends BaseModel<ID>, ID> {
     }
 
     public static boolean validEmail(String email) {
-//        return email.equalsIgnoreCase("/^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i");
         return email.contains("@");
     }
 
     public static boolean onlyNumbers(String numbers) {
         return numbers.equalsIgnoreCase("/^\\d{1,}$/");
-        //re.test('13'); // true
-        //re.test('23yy'); // false
-    }
-
-    public static boolean validNumberPhone(String numberPhone) {
-        return numberPhone.equalsIgnoreCase(
-                "/^\\+?(\\d{1,3})?[- .]?\\(?(?:\\d{2,3})\\)?[-" +
-                        " .]?\\d\\d\\d[- .]?\\d\\d\\d\\d$/");
-        //re.test('(212) 348-2626'); // true
-        //re.test('+1 832-393-1000'); // true
-        //re.test('+1 202-456-11-11'); // false
-    }
-
-    public static boolean goodPassword(String password) {
-        return password.equalsIgnoreCase(
-                "/^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])" +
-                        "(?=.*[a-z].*[a-z].*[a-z]).{8,}$/");
-        //re.test('qwerty'); // false
-        //re.test('qwertyuiop'); // false
-        //re.test('abcABC123$'); // true
-    }
-
-    public static boolean goodDate(String date) {
-        return date.equalsIgnoreCase("/^(?:(?:31(\\/|-|\\.)(?:0?[13578]|1[02]))\\1|(?:(?:29|30)(\\/|-|\\.)" +
-                "(?:0?[1,3-9]|1[0-2])\\2))(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$|^(?:29(\\/|-|\\.)0?2\\3(?:(?:(?:1[6-9]|[2-9]\\d)" +
-                "?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\\d|2[0-8])(\\/|-|\\.)" +
-                "(?:(?:0?[1-9])|(?:1[0-2]))\\4(?:(?:1[6-9]|[2-9]\\d)?\\d{2})$/");
-        //re.test('29-02-2000'); // true
-        //re.test('29-02-2001'); // false
-    }
-
-    public static boolean urlOrNot(String url) {
-        return url.equalsIgnoreCase("/[-a-zA-Z0-9@:%_\\+.~#?&\\/=]{2,256}\\.[a-z]{2,4}\\b(\\/" +
-                "[-a-zA-Z0-9@:%_\\+.~#?&\\/=]*)?/gi");
-        //re.test('https://yandex.ru'); // true
-        //re.test('yandex.ru'); // true
-        //re.test('hello world'); // false
-    }
-
-    public static boolean takeOnlyDomain(String domain) {
-        String getDomainName = "/https?:\\/\\/(?:[-\\w]+\\.)?([-\\w]+)\\.\\w+(?:\\.\\w+)?\\/?.*/i";
-
-        boolean matches = domain.matches(getDomainName);
-        return domain.equalsIgnoreCase(
-                "/https?:\\/\\/(?:[-\\w]+\\.)?([-\\w]+)\\.\\w+(?:\\.\\w+)?\\/?.*/i");
-        //let domain = 'https://proglib.io'.match(re);
-        //console.log(domain[1]); // proglib
     }
 }
 
